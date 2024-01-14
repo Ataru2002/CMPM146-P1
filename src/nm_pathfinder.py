028f54fef46d2334bf5b25f4dc4611781882aa60
Nhan <nhtrnguy@ucsc.edu>
2024-01-14 13:29:14 -0800

    finishing up tester

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -1,6 +1,7 @@
 import math
 import heapq
 from queue import PriorityQueue
+from queue import Queue
 
 def find_path (source_point, destination_point, mesh):
 
@@ -20,6 +21,7 @@ def find_path (source_point, destination_point, mesh):
     start = 0
     path = []
     boxes = {}
+    ranges = {}
     detail_points = {}
     source_box = ()
     destination_box = ()
@@ -36,19 +38,24 @@ def find_path (source_point, destination_point, mesh):
                 if key[0] <= destination_point[0] and destination_point[0] <= key[1] and key[2] <= destination_point[1] and destination_point[1] <= key[3]:
                     destination_box = key
         start += 1
+
+    #debug zone
+    #print(detail_points[destination_box])
+    #print(destination_box)
+    #print(box_graph[destination_box])
+    #print(destination_point)
+    #print(source_point)
+        
     if source_box == () or destination_box == ():
         print("No path!")
         return path, boxes.keys()
-    point_construction(detail_points, box_point, box_graph, source_box, source_point)
+    
+    range_construction(ranges, box_graph)
+    
     box_point[source_point] = source_box
     box_point[destination_point] = destination_box
     path_table = {}
-    #print(detail_points[destination_box])
-    print(destination_box)
-    print(box_graph[destination_box])
-    print(destination_point)
-    print(source_point)
-    A_star(detail_points, path_table, box_graph, source_box, source_point, destination_box, destination_point)
+    A_star(detail_points, ranges, path_table, box_graph, box_point, source_box, source_point, destination_box, destination_point)
     starter = destination_point
     test = 0
     
@@ -59,117 +66,83 @@ def find_path (source_point, destination_point, mesh):
     
     print(path)
     if starter != source_point:
-        #print(starter)
-        #print(destination_point)
-        #print(source_point)
-        #print(path)
         print("No path!")
         return path, boxes.keys()
     
     boxes[box_point[starter]] = box_point[starter]
     path.insert(0, starter)
+    #path = []
     return path, boxes.keys()
-
-# tmr finish up dijkstra and A*
 
 def euclidian_distance(u, v):
     return math.sqrt(((u[0] - v[0]) * (u[0] - v[0])) + (((u[1] - v[1]) * (u[1] - v[1]))))
 
-def A_star(detail_points, path_table, box_graph, source_box, source_point, destination_box, destination_point):
+def A_star(detail_points, ranges, path_table, box_graph, box_point, source_box, source_point, destination_box, destination_point):
     dist = {}
-    #heap = []
     heap = PriorityQueue()
     for i in detail_points.values():
-        dist[i] = 1000000000
+        dist[i] = math.inf
         path_table[i] = -1
-    #heapq.heapify(heap)
-    #heapq.heappush(heap, (0, source_box, source_point))
+    
+    detail_points[source_box] = source_point
+    box_point[source_point] = source_box
     heap.put((0, source_box, source_point))
     dist[source_point] = 0
 
     while not heap.empty():
-        #current_point = heap[0]
-        #heapq.heappop(heap)
         current_point = heap.get()
 
-        #print(current_point)
-        #print(heap)
+        if current_point[0] != dist[current_point[2]]: 
+           continue
 
         if current_point[1] == destination_box:
             path_table[destination_point] = current_point[2]
             print(destination_point)
-            break
-
-        #if current_point[0] != dist[current_point[2]]: 
-        #   continue
+            break        
 
         for i in box_graph[current_point[1]]:
+            relationship = (current_point[1], i)
+            x_range = ranges[relationship][0]
+            y_range = ranges[relationship][1]
             point1 = current_point[2]
-            point2 = detail_points[i]
-            cost_current = euclidian_distance(point1, point2) + dist[point1] 
-            #+ euclidian_distance(point2, destination_point)
+            point2 = point_identifier(point1, i, detail_points, box_point, x_range, y_range)
+            cost_current = euclidian_distance(point1, point2) + dist[point1] + euclidian_distance(point2, destination_point)
             if point2 not in dist or cost_current < dist[point2]:
                 dist[point2] = cost_current
                 path_table[point2] = point1
-                #heapq.heappush(heap, (dist[point2], i, detail_points[i]))
                 heap.put((dist[point2], i, detail_points[i]))
 
-def point_construction(detail_points, box_point, box_graph, source_box, source_point):
+def range_construction(ranges, box_graph):
     # construction
-    # can be slow so keep this in mind
-    queue = []
+    for i in box_graph:
+        for j in box_graph[i]:
+            current_box = i
+            adj_box = j
+            x_range = (max(current_box[0], adj_box[0]), min(current_box[1], adj_box[1]))
+            y_range = (max(current_box[2], adj_box[2]), min(current_box[3], adj_box[3]))
+            ranges[(i, j)] = (x_range, y_range)
+
+def point_identifier(current_point, adj_box, detail_points, box_point, x_range, y_range):
+    detail_point = ()
+
+    # horizontal range    
+    if y_range[0] == y_range[1] and x_range[0] <= current_point[0] and current_point[0] <= x_range[1]:
+        detail_point = (current_point[0], y_range[0])
+    elif y_range[0] == y_range[1] and current_point[0] <= x_range[0]:
+        detail_point = (x_range[0], y_range[0])
+    elif y_range[0] == y_range[1] and current_point[0] >= x_range[1]:
+        detail_point = (x_range[1], y_range[0])
     
-    visited = {}
-    for i in box_graph.keys():
-        visited[i] = False
 
-    queue.append(source_box)
-    detail_points[source_box] = source_point
-    visited[source_box] = True
+    # vertical range
+    if x_range[0] == x_range[1] and y_range[0] <= current_point[1] and current_point[1] <= y_range[1]:
+        detail_point = (x_range[0], current_point[1])
+    elif x_range[0] == x_range[1] and current_point[1] <= y_range[0]:
+        detail_point = (x_range[0], y_range[0])
+    elif x_range[0] == x_range[1] and current_point[1] >= y_range[1]:
+        detail_point = (x_range[0], y_range[1])
 
-    while len(queue) > 0: 
-        current_box = queue[0]
-        current_point = detail_points[current_box]
-        queue.pop(0) # takes O(n) so keep this in mind
-        for adj_box in box_graph[current_box]:
-            # for each box determine the detail points from the current_box
-
-            if visited[adj_box] == False:
-                detail_point = ()
-
-                visited[adj_box] = True
-                y_range = (max(current_box[2], adj_box[2]), min(current_box[3], adj_box[3]))
-                x_range = (max(current_box[0], adj_box[0]), min(current_box[1], adj_box[1]))
-                
-
-                # this might be wrong so beware
-                # horizontal range
-                
-                if y_range[0] == y_range[1] and x_range[0] <= current_point[0] and current_point[0] <= x_range[1]:
-                    detail_point = (current_point[0], y_range[0])
-                elif y_range[0] == y_range[1] and current_point[0] <= x_range[0]:
-                    detail_point = (x_range[0], y_range[0])
-                elif y_range[0] == y_range[1] and current_point[0] >= x_range[1]:
-                    detail_point = (x_range[1], y_range[0])
-                
-
-                # vertical range
-                if x_range[0] == x_range[1] and y_range[0] <= current_point[1] and current_point[1] <= y_range[1]:
-                    detail_point = (x_range[0], current_point[1])
-                elif x_range[0] == x_range[1] and current_point[1] <= y_range[0]:
-                    detail_point = (x_range[0], y_range[0])
-                elif x_range[0] == x_range[1] and current_point[1] >= y_range[1]:
-                    detail_point = (x_range[0], y_range[1])
-                '''
-                print('\n')
-                print(current_box)
-                print(adj_box)
-                print(current_point)
-                print(detail_point)
-                print(x_range)
-                print(y_range)
-                print('\n')
-                '''
-                detail_points[adj_box] = detail_point
-                box_point[detail_point] = adj_box
-                queue.append(adj_box)
+    detail_points[adj_box] = detail_point
+    box_point[detail_point] = adj_box
+    current_point = detail_point
+    return detail_point
